0a9e0d9f6e3a13a0f11f10d8df5e84adf6ea0ad3
Emmanuelle Sadone <emmanuellesadone@gmail.com>
2015-11-06 11:03:22 +0100

    seeed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: 'emma@sdn.com', encrypted_password: '123456789')
-Annonce.create()
+
+Annonce.create(name: 'Elsa Wild', child_number: 3, adresse: '32 bd saint germain', city: 'paris', price: 25,  description: 'Animatrice dans un périscolaire et centre de loisirs je suis acellement en mise en disponibilité pour un an. Je suis à la recherche d une garde d enfants à domicile n ayant pas encore fait ma demande d agrément je suis donc considéré comme baby sitter',)
+Annonce.create(name: 'Mathilde Cochet', child_number: 3, adresse: '24 cours de l ile Seguin', city: 'paris', price: 15,  description: 'Animatrice dans un périscolaire et centre de loisirs je suis acellement en mise en disponibilité pour un an. Je suis à la recherche d une garde d enfants à domicile n ayant pas encore fait ma demande d agrément je suis donc considéré comme baby sitter',)
+Annonce.create(name: 'Margaux Savy', child_number: 3, adresse: '44 rue Pauline Borghese', city: 'Neuilly-sur-Seine', price: 20,  description: 'Animatrice dans un périscolaire et centre de loisirs je suis acellement en mise en disponibilité pour un an. Je suis à la recherche d une garde d enfants à domicile n ayant pas encore fait ma demande d agrément je suis donc considéré comme baby sitter',)
+Annonce.create(name: 'Audrey Achache', child_number: 3, adresse: '14 rue du four', city: 'paris', price: 25,  description: 'Animatrice dans un périscolaire et centre de loisirs je suis acellement en mise en disponibilité pour un an. Je suis à la recherche d une garde d enfants à domicile n ayant pas encore fait ma demande d agrément je suis donc considéré comme baby sitter',)
+Annonce.create(name: 'Basma Tahir', child_number: 3, adresse: '4 Villa Gaudelet', city: 'paris', price: 15,  description: 'Animatrice dans un périscolaire et centre de loisirs je suis acellement en mise en disponibilité pour un an. Je suis à la recherche d une garde d enfants à domicile n ayant pas encore fait ma demande d agrément je suis donc considéré comme baby sitter',)
+Annonce.create(name: 'Alexandra Maubouss', child_number: 3, adresse: '74 bd saint germain', city: 'paris', price: 20,  description: 'Animatrice dans un périscolaire et centre de loisirs je suis acellement en mise en disponibilité pour un an. Je suis à la recherche d une garde d enfants à domicile n ayant pas encore fait ma demande d agrément je suis donc considéré comme baby sitter',)
+Annonce.create(name: 'Emma Sadone', child_number: 3, adresse: '16 Villa Gaudelet', city: 'paris', price: 25,  description: 'Animatrice dans un périscolaire et centre de loisirs je suis acellement en mise en disponibilité pour un an. Je suis à la recherche d une garde d enfants à domicile n ayant pas encore fait ma demande d agrément je suis donc considéré comme baby sitter',)
+
+
+
+
